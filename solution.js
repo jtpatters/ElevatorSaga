f9add2dfc55e2831b36e04c435a69b553148f0e4
Jason Patterson <jtpatters@gmail.com>
2023-03-06 22:13:56 -0500

    fixed the twinkling lights

--- solution.js
+++ solution.js
@@ -1,44 +1,53 @@
 {
-    
+
     init: function(elevators, floors) {
-        
-        function stopElevator(elevator,floorNum){
-            console.log("stopping at floor: "+floorNum);
-            console.log("destingation queue prior:"+elevator.destinationQueue);
-            elevator.destinationQueue.splice(elevator.destinationQueue.indexOf(floorNum), 1);
-            elevator.goToFloor(floorNum, true); 
+
+        function stopElevator(elevator,floor){
+            elevator.destinationQueue.splice(elevator.destinationQueue.indexOf(floor.floorNum()), 1);
+            elevator.goToFloor(floor.floorNum(), true); 
             elevator.checkDestinationQueue();
-            console.log("destingation queue after:"+elevator.destinationQueue);
         }
         
+        function clearFloor(floor){
+            floor.requestedUp = false;
+            floor.requestedDown = false;
+        }
+        
+        function goingUp(elevator,floor){
+            elevator.goingUpIndicator(true);
+            elevator.goingDownIndicator(false);
+            floor.requestedUp = false;
+            stopElevator(elevator,floor);
+        }
+        
+        function goingDown(elevator,floor){
+            elevator.goingUpIndicator(false);
+            elevator.goingDownIndicator(true);
+            floor.requestedDown = false;
+            stopElevator(elevator,floor);
+        }
+
         elevators.forEach(function(elevator, index) {
-        
-        elevator.on("floor_button_pressed", function(floorNum) {  
-            if (elevator.destinationQueue.indexOf(floorNum) == -1) {
-                elevator.goToFloor(floorNum); 
-            }
-        });
-       
-        elevator.on("passing_floor", function(floorNum, direction) { 
-            console.log("passing floor:"+floorNum+" going direction:"+direction)
-            if (elevator.loadFactor() >= 0.95){
-                console.log("elevator passing - too full");
-            }else if(floors[floorNum].buttonStates.down && direction == "down"){
-                elevator.goingDownIndicator(true);
-                elevator.goingUpIndicator(false);
-                floors[floorNum].buttonStates.down = "";
-                stopElevator(elevator,floorNum);
-            }else if(floors[floorNum].buttonStates.up && direction == "up"){
-                elevator.goingUpIndicator(true);
-                elevator.goingDownIndicator(false);
-                floors[floorNum].buttonStates.up = "";
-                stopElevator(elevator,floorNum);
-            }else if (elevator.destinationQueue.indexOf(floorNum) != -1) { 
-                stopElevator(elevator,floorNum);
-            }
-        });
-        
-        
+
+            elevator.on("floor_button_pressed", function(floorNum) {  
+                if (elevator.destinationQueue.indexOf(floorNum) == -1) {
+                    elevator.goToFloor(floorNum); 
+                }
+            });
+
+            elevator.on("passing_floor", function(floorNum, direction) { 
+                console.log("passing floor:"+floorNum+" going direction:"+direction)
+                if (elevator.loadFactor() >= 0.85){
+                    console.log("elevator passing - too full");
+                }else if(floors[floorNum].buttonStates.down && direction == "down"){
+                    goingDown(elevator,floors[floorNum]);    
+                }else if(floors[floorNum].buttonStates.up && direction == "up"){
+                    goingUp(elevator,floors[floorNum]);
+                }else if (elevator.destinationQueue.indexOf(floorNum) != -1) { 
+                    stopElevator(elevator,floors[floorNum]);
+                }
+            });
+
             elevator.on("idle", function() {
                 elevator.goingUpIndicator(true);
                 elevator.goingDownIndicator(true);
@@ -55,26 +64,23 @@
                                 min_distance=distance;
                                 min_floor=demand[i];
                             }                  
-                            console.log("demand includes:"+demand[i].floorNum())
                         }
                     }
                     if (min_floor) {
                         target = min_floor.floorNum();
-                        floors[target].buttonStates.up = "";
-                        floors[target].buttonStates.down = "";
+                        clearFloor(min_floor);
                         console.log("heading to floor: "+target);
                     } else {
                         target = 0;
                         console.log("going to the lobby");
                     }
                     elevator.goToFloor(target);
-                   
                 }
             });
         });
-        
+
     },
-    update: function(dt, elevators, floors) {
+        update: function(dt, elevators, floors) {
             // We normally don't need to do anything here
-    }
+        }
 }
