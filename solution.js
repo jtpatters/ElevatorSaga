4cb63c13d20bd7d0d13e6ac2822ce5d3f655e7e4
Jason Patterson <jtpatters@gmail.com>
2023-03-09 00:04:15 -0500

    introduced express mode
    still struggling on level 13...

--- solution.js
+++ solution.js
@@ -3,13 +3,16 @@
     init: function(elevators, floors) {
 
         function stopElevator(elevator,floor,index){
-            console.log("e:"+index+" prep unscheduled stopping at:"+floor.floorNum()+" q:"+elevator.destinationQueue);
-            if(elevator.destinationQueue.indexOf(floor.floorNum())>-1){
-                elevator.destinationQueue.splice(elevator.destinationQueue.indexOf(floor.floorNum(), 1));
+            var floor_num=floor.floorNum();
+            console.log("e:"+index+" prep unscheduled stopping at:"+floor_num+" q:"+elevator.destinationQueue);
+            var floor_index = elevator.destinationQueue.indexOf(floor_num);
+            if(floor_index>-1){
+                elevator.destinationQueue.splice(floor_index,1);
                 elevator.checkDestinationQueue();
+                console.log("called splice with index: "+floor_index);
             }
             elevator.goToFloor(floor.floorNum(), true); 
-            console.log("e:"+index+" done prep unschedule stopping at:"+floor.floorNum()+" q:"+elevator.destinationQueue);
+            console.log("e:"+index+" done prep unschedule stopping at:"+floor_num+" q:"+elevator.destinationQueue);
            
         }
 
@@ -35,6 +38,8 @@
         });       
 
         elevators.forEach(function(elevator, index) {
+            
+            elevator.mode="normal";
 
             elevator.on("stopped_at_floor", function(floorNum) {
                 floors[floorNum].enRoute=false;
@@ -57,54 +62,70 @@
 
             elevator.on("passing_floor", function(floorNum, direction) { 
                 console.log("e:"+index+" passing:"+floorNum+" q:"+elevator.destinationQueue);
-                if (elevator.loadFactor() > 0.85){
-                    console.log("e:"+index+" passing - too full");
+                if (elevator.destinationQueue.indexOf(floorNum) != -1) { 
+                    stopElevator(elevator,floors[floorNum],index);
+                }else if (elevator.mode=="express"){
+                    console.log("e:"+index+" in express mode :)");
+                }else if(elevator.loadFactor() > 0.75 || elevator.destinationQueue.length>2){
+                    elevator.mode="express";
+                    console.log("e:"+index+" passing - too full or queue too long...");
                 }else if(floors[floorNum].buttonStates.down && direction == "down"){
                     goingDown(elevator,floors[floorNum],index);    
                 }else if(floors[floorNum].buttonStates.up && direction == "up"){
                     goingUp(elevator,floors[floorNum],index);
-                }else if (elevator.destinationQueue.indexOf(floorNum) != -1) { 
-                    stopElevator(elevator,floors[floorNum],index);
                 }
+                
                 console.log("e:"+index+" completed pass:"+floorNum+" q:"+elevator.destinationQueue);
             });
 
             elevator.on("idle", function() {
                 elevator.goingUpIndicator(true);
                 elevator.goingDownIndicator(true);
+                elevator.mode="normal";
                 if (elevator.loadFactor()>0){
-                    console.log("e:"+index+" IS IDLE AND NOT EMPTY (l:"+elevator.loadFactor()+"... STUCK!");
+                    //this should never happen, but sometimes does... send directionless elevator to lobby
+                    console.log("e:"+index+" IS IDLE AND NOT EMPTY (l:"+elevator.loadFactor()+"... STUCK!!!!!");
                     elevator.goingUpIndicator(false);
                     elevator.goingDownIndicator(true);
                     elevator.goToFloor(0);       
                 } else {
-                    var target;
+                    var distance_target, wait_target=floors[0];
                     var min_distance=99;
+                    var max_wait=0;
                     var current_date=new Date();
                     for(var i=0; i<floors.length;i++){
                         if(floors[i].buttonStates.down || floors[i].buttonStates.up){
                             if(floors[i].enRoute){
                                 continue;
                             }
-                            if(current_date - floors[i].lastPress>3000){
-                                target=floors[i];
-                                break;
+                            var wait=current_date - floors[i].lastPress;
+                            if(wait>max_wait){
+                                max_wait=wait;
+                                wait_target=floors[i];
                             } 
                             var distance=Math.abs(elevator.currentFloor()-floors[i].floorNum());
                             if(distance<min_distance){
                                 min_distance=distance;
-                                target=floors[i];
+                                distance_target=floors[i];
                             }
                         }
                     }
-                    if (target) {
-                        target.enRoute=true;
-                        elevator.goToFloor(target.floorNum());
-                        console.log("e:"+index+" non-idle-heading to floor: "+target.floorNum());
+                    var target;
+                    console.log("max wait: "+max_wait+" min distance: "+min_distance);
+                    if(max_wait>1000){
+                        target=wait_target;
+                        console.log("Setting target based on wait. f:"+target.floorNum());
+                    } else if(min_distance==1){
+                        target=distance_target;
+                        console.log("Setting target based on distance. f:"+target.floorNum());
                     } else {
-                        elevator.goToFloor(0);
+                        target=wait_target;
+                        console.log("Setting target based on wait. f:"+target.floorNum());
                     }
-
+                    
+                    target.enRoute=true;
+                    elevator.goToFloor(target.floorNum());
+                    
                 }
             });
         });
